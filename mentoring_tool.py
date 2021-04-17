6ca9d5e23ad478f5538dbeae1af9bff628f1081f
Helena Rosengarten <helenarosengarten@Helenas-MacBook.fritz.box>
2021-04-17 17:01:13 +0200

    first commit

--- mentoring_tool.py
+++ mentoring_tool.py
@@ -16,16 +16,25 @@ def main():
 def write_excel():
   # Columns: Name, Email, Uni, Studiengang, Semester, Promotion/Studium, #Semester in der Stiftung, Will gleichen Studiengang,
 
-  file_exists = os.path.isfile("mentoring_input.csv")
+  file_exists = os.path.isfile("mentee_input.csv") 
   print(file_exists)
 
   # write to csv
-  with open('mentoring_input.csv', 'a', newline='') as csvfile:
+  with open('mentee_input.csv', 'a', newline='') as csvfile:
     mentoring_data_writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
     if not file_exists:
       mentoring_data_writer.writerow(['Name', 'Email', 'Uni', 'Studiengang', 'Semester', 'Promotion/Studium, Semester_in_der_Stiftung'])
     mentoring_data_writer.writerow(["test0", "test1", "test2", "test3", "test4", "test5"])
 
+  file_exists = os.path.isfile("mentor_input.csv") 
+  print(file_exists)
+
+  # write to csv
+  with open('mentor_input.csv', 'a', newline='') as csvfile:
+    mentoring_data_writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    if not file_exists:
+      mentoring_data_writer.writerow(['Name', 'Email', 'Uni', 'Studiengang', 'Semester', 'Promotion/Studium, Semester_in_der_Stiftung'])
+    mentoring_data_writer.writerow(["test0", "test1", "test2", "test3", "test4", "test5"])
   return
 
 
